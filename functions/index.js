16784562550c9e8b7be14d667be4505a882bdf93
Flor <florxgomez@gmail.com>
2019-10-12 19:05:09 -0300

    authentication middleware

--- functions/index.js
+++ functions/index.js
@@ -47,17 +47,50 @@ app.get("/posts", (req, res) => {
     .catch(err => console.error(err));
 });
 
+//Authentication middleware
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized " });
+  }
 
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next(); //allows the request to proceed
+    })
+    .catch(err => {
+      console.error("Error while verifying token ", err);
+      return res.status(400).json(err);
+    });
+};
 
 //Post a post
-app.post("/post", (req, res) => {
+app.post("/post", FBAuth, (req, res) => {
   if (req.body.body.trim() === "") {
     return res.status(400).json({ body: "Body must not be empty" });
   }
 
   const newPost = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString(),
   };
 
